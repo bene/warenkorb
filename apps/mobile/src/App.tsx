422eee1e73b8dd067d6f4705363e072109b3e15b
Benedict Allerberger <me@bene.dev>
2023-07-29 19:49:53 +0200

    Setup nativewind

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,10 +1,14 @@
+import "./global.css";
+
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text className="text-3xl font-bold">
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
